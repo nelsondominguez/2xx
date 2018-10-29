55d2a95287e35da9a28e9f3f496d5cf352974c67
nelsondominguez <nelsonjr73090@gmail.com>
2018-10-29 01:59:16 -0400

    Completed menu functionality

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,9 +1,8 @@
 <footer>
-        <h4>this is the footer</h4>
-        <p>we place redundant information here</p>
-        <div id="footerTagLine">I am 7 inches left of normal</div>
-    </footer>
-
-</body>
-
+               <h4>this is the footer</h4>
+               <p>we place redundant information here</p>
+               <div id="footerTagLine">I am 7 inches left of normal</div>
+               <div class="copyright">iop project - Version <?php echo VERSION; ?> | &copy; 1990 - <?php echo date('Y'); ?> Nelson Dominguez Jr.</div>
+          </footer>
+     </body>
 </html>
--- include/header.inc.php
+++ include/header.inc.php
@@ -1,86 +1,73 @@
 <?php
-	
-	// Pulls data element used through out the entire website
-     require_once 'menu.data.php';
+     // Pulls data information and configurations used through out the application
+     require_once 'config.inc.php';
      
-     // Pulls functions used through out the entire website
+     // Pulls data elements used through out the entier website
+     require_once 'content.data.php';
+     
+     // Pulls functions used through out the entier website
      require_once 'functions.inc.php';
-
-	/*
-		The following will be passed along to JavaScript by establising variables in PHP and passing them into the HTML via Global JavaScript variables the same names used for PHP
-	*/
-
-	$siteName = 'inside out | 2xx';
-
+	if($_SERVER["SERVER_PORT"] == '80' && $_SERVER["HTTP_HOST"] !== 'localhost'){
+       header('Location: https://'.$_SERVER["HTTP_HOST"]);
+     }
 ?>
 
 <!doctype html>
 
 <?php
-
-	/* 
-		this is beginning of the html the browser will process 
-	*/
-
+//this is begining of the html the browser will process
 ?>
 
 <html>
-	
-<?php
-	
-	/*
-		information used by the browser
-	*/
-?>
-	
-<head>
-
-    <meta charset="utf-8">
-	
-	<?php
-	
-		/*
-    		displayed anywhere the title is used for the browser, search engines, etc. 
-		*/
-	?>
-		
-
-    <title><?php echo $siteName; ?></title>
-	
-	<?php
-	
-		/*
-    		used to display information about the page by search engines and other sites looking at our page.
-		*/
-	
-	?>
-	
-    <meta name="description" content="inside out is a class project we use to learn html." />
-
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
-		 <script>
-               var siteName = '<?php echo $siteName; ?>';
-          </script>
-
-</head>
 
 <?php
-
-	/*
-		content displayed in the browser window
-	*/
+//information used by the browser
 ?>
-	
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<?php
+		/*
+			 displayed anywhere the title is used
+			 for the browser, search engines, etc.
+		*/
+		?>
+
+		<title>
+			<?php echo SITENAME; ?>
+		</title>
+
+		<?php
+		/*
+			 used to display information about the
+			 page by search engines and other sites
+			 looking at our page.
+		*/
+		?>
+
+		<meta name="description" content="inside out is a class project we use to learn html."/>
+
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+		<script>
+			var siteName = '<?php echo SITENAME; ?>';
+		</script>
+
+	</head>
+
+	<?php
+	//contnet displayed in the browser window
+	?>
 
 <body>
 
-    <header>
-        <h1><a href="index.php"><?php echo $siteName; ?></a></h1>
-        <h2>continuously falling forward in to the light...</h2>
+	<header>
+		<h1><a href="index.php"><?php echo SITENAME; ?></a></h1>
+		<h2>continuously falling forward in to the light...</h2>
 		
-			<nav>
-        		<?php echo menuBuilder($menuItems); ?>
-			</nav>
-		
-    </header>
+		  <nav>
+			     <?php echo menuBuilder($content['pages']); ?>
+          </nav>
+	</header>
